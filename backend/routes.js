f4b1213961b5ccd06d4c041ff22a8b78a61eb98c
Seun Suberu <seunsuberu@Seuns-MacBook-Pro.local>
2021-03-28 16:07:57 -0500

    Created Sample Routes with description of purpose

--- backend/routes.js
+++ backend/routes.js
@@ -1,3 +1,4 @@
+const { off } = require('./db');
 const pool = require('./db')
 
 module.exports = function routes(app, logger) {
@@ -94,5 +95,230 @@ module.exports = function routes(app, logger) {
         });
       }
     });
+    
+  });
+
+  // POST /api/createUser
+  //sign in api to create new user
+  app.post('/api/createUser', (req, res) => {
+    // obtain a connection from our pool of connections
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection'); 
+      } else {
+        let firstName = req.body["firstName"];
+        let lastName = req.body["lastName"];
+        let userEmail = req.body["userEmail"];
+        let userPassword = req.body["userPassword"];
+        let exposure = req.body["exposure"];
+        let jobTitle = req.body["jobTitle"];
+        let officeId = req.body["officeId"];
+        let insert = [
+          [firstName, lastName, userEmail, userPassword, exposure, jobTitle, officeId, ]
+        ];
+        let sql = 'INSERT INTO users(firstName, lastName, userEmail, userPassword, exposure, jobTitle, officeId) VALUES ?';
+        console.log(sql);
+        // if there is no issue obtaining a connection, execute query and release connection
+        connection.query(sql, [insert], function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            logger.error("Error while fetching values: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining values"
+            })
+          } else {
+            res.status(200).json({
+              "data": rows
+            });
+          }
+        });
+      }
+    });
+    
+  });
+
+  // GET /api/login
+  //authentification route, returns 0 if successful login, 1 if user doesn't exist, and 2 if incorrect password
+  app.get('/api/login', async(req, res) => {
+    // obtain a connection from our pool of connections
+    pool.getConnection(async function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection'); 
+      } else {
+        let userEmail = req.query["userEmail"];
+        let userPassword = req.body["userPassword"];
+        let sql1 = 'SELECT userId FROM users WHERE userEmail =\'' + userEmail + '\'';
+        let sql2 = 'SELECT userId FROM users WHERE userEmail =\'' + userEmail + '\' AND ' + 'userPassword = \'' + userPassword + '\'';
+        console.log(sql1, sql2)
+        let validEmail = false;
+        connection.query(sql1, function (err, rows, fields) {
+          if (err) {
+            logger.error("Error while fetching values: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining values"
+            })
+          } else {
+            console.log(rows.length)
+            if(rows.length > 0){
+              console.log("change")
+              // if there is no issue obtaining a connection, execute query and release connection
+              connection.query(sql2, function (err, rows, fields) {
+                connection.release();
+                if (err) {
+                  logger.error("Error while fetching values: \n", err);
+                  res.status(400).json({
+                    "data": [],
+                    "error": "Error obtaining values"
+                  })
+                } else {
+                  res.status(200).json({"status" : rows.length > 0 ? 0 : 2});
+                }
+              });
+            }
+            else{
+              res.status(200).json({"status" : 1});
+            }
+          }
+        });     
+      }
+    });
+    
+  });
+
+  // GET /api/user
+  //get information about specific user
+  app.get('/api/user', (req, res) => {
+    // obtain a connection from our pool of connections
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection'); 
+      } else {
+        let userId = req.query["userId"];
+        let sql = 'SELECT * FROM users WHERE userId = ?';
+        // if there is no issue obtaining a connection, execute query and release connection
+        connection.query(sql, userId, function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            logger.error("Error while fetching values: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining values"
+            })
+          } else {
+            res.status(200).json({
+              "data": rows
+            });
+          }
+        });
+      }
+    });
+  });
+
+
+
+  // POST /api/createOffice
+  //creates new Office
+  app.post('/api/createOffice', (req, res) => {
+    // obtain a connection from our pool of connections
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection'); 
+      } else {
+        let city = req.body["city"];
+        let state = req.body["state"];
+        let countryCode = req.body["countryCode"];
+        let insert = [
+          [city, state, countryCode]
+        ];
+        let sql = 'INSERT INTO offices(city, state, countryCode) VALUES ?';
+        // if there is no issue obtaining a connection, execute query and release connection
+        connection.query(sql, [insert], function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            logger.error("Error while fetching values: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining values"
+            })
+          } else {
+            res.status(200).json({
+              "data": rows
+            });
+          }
+        });
+      }
+    });
+    
+  });
+
+  // GET /api/office
+  //get specific information about an office
+  app.get('/api/office', (req, res) => {
+    // obtain a connection from our pool of connections
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection'); 
+      } else {
+        let officeid = req.query["officeId"];
+        let sql = 'SELECT * FROM offices WHERE officeId = ?';
+        // if there is no issue obtaining a connection, execute query and release connection
+        connection.query(sql, officeid, function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            logger.error("Error while fetching values: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining values"
+            })
+          } else {
+            res.status(200).json({
+              "data": rows
+            });
+          }
+        });
+      }
+    });
+  });
+    // get /api/offices
+    // get all office information
+  app.get('/api/offices', (req, res) => {
+    // obtain a connection from our pool of connections
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection'); 
+      } else {
+        let sql = 'SELECT * FROM offices';
+        // if there is no issue obtaining a connection, execute query and release connection
+        connection.query(sql, function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            logger.error("Error while fetching values: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining values"
+            })
+          } else {
+            res.status(200).json({
+              "data": rows
+            });
+          }
+        });
+      }
+    });
+    
   });
 }
