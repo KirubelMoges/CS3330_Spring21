07838441f8cfd59b097c32430d0e69c0a2ee0630
Nick Wall <walln@smu.edu>
2021-04-08 15:04:12 -0500

    Fixed employee schedule modal to have room selector

--- frontend/src/components/calendar-view/employee-view.js
+++ frontend/src/components/calendar-view/employee-view.js
@@ -3,10 +3,12 @@ import './styles/calendar.css';
 import React, { useState } from 'react';
 import { getDays } from './utils';
 import { RoomCard, TimeCard } from './cards';
-import { Modal } from 'react-bootstrap';
+import { Modal, Form } from 'react-bootstrap';
 
 const EmployeeView = () => {
   const days = getDays(new Date());
+
+  const rooms = [1, 2, 3, 4, 5];
 
   const [isScheduleShowing, setIsScheduleShowing] = useState(false);
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -16,8 +18,8 @@ const EmployeeView = () => {
   };
   const handleCloseSchedule = () => setIsScheduleShowing(false);
 
-  const handleReserve = (day) => {
-    console.log(day);
+  const handleReserve = (day, room) => {
+    console.log(day, room);
     handleCloseSchedule();
   };
 
@@ -43,9 +45,6 @@ const EmployeeView = () => {
             return (
               <li key={day} onClick={() => handleOpenSchedule(day)}>
                 <div className="date">{day.getDate()}</div>
-                {(day.getDate() < 2 || day.getDate() === 11) && (
-                  <div className="event bg-primary">There is an event today in room 2001</div>
-                )}
               </li>
             );
           })}
@@ -58,12 +57,16 @@ const EmployeeView = () => {
         handleClose={handleCloseSchedule}
         date={selectedDate}
         handleReserve={handleReserve}
+        rooms={rooms}
       />
     </div>
   );
 };
 
 const ScheduleModal = (props) => {
+  const rooms = [...props.rooms];
+  const [selectedRoom, setSelectedRoom] = useState(rooms[0]);
+
   return (
     <Modal show={props.show} onHide={props.handleClose}>
       <Modal.Header closeButton>
@@ -71,9 +74,22 @@ const ScheduleModal = (props) => {
       </Modal.Header>
       <Modal.Body>
         <p>Selected Date: {props.date.toDateString()}</p>
+        <Form>
+          <Form.Group controlId="scheduleForm.room">
+            <Form.Label>Room Number</Form.Label>
+            <Form.Control as="select" onChange={(e) => setSelectedRoom(e.target.value)}>
+              {rooms.map((room) => (
+                <option key={room}>{room}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+        </Form>
       </Modal.Body>
       <Modal.Footer>
-        <button className="btn btn-success" onClick={() => props.handleReserve(props.date)}>
+        <button
+          className="btn btn-success"
+          onClick={() => props.handleReserve(props.date, selectedRoom)}
+        >
           Reserve
         </button>
         <button className="btn btn-danger" onClick={props.handleClose}>
