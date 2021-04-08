69289e02615ebf0482d9653266d2db751db5441d
Nick Wall <walln@smu.edu>
2021-04-08 12:52:12 -0500

    Added time-change button

--- frontend/src/components/calendar-view/cards/modals/time-stats.js
+++ frontend/src/components/calendar-view/cards/modals/time-stats.js
@@ -10,8 +10,14 @@ const TimeStatsModal = (props) => {
         <p>Here is the breakdown of your time:</p>
         <p>Time today: 3hrs</p>
         <p>Time this week: 22hrs</p>
+        <p>Events:</p>
+        <p>Clocked in today at: 9am</p>
+        <p>Clocked out at: Have not clocked out yet.</p>
       </Modal.Body>
       <Modal.Footer>
+        <button className="btn btn-info" onClick={() => alert('Request sent.')}>
+          Request a time-change
+        </button>
         <button className="btn btn-secondary" onClick={props.handleClose}>
           Close
         </button>
