0dfd3004b7491081d40de4c7592250b86fedbab0
Seun Suberu <seunsuberu@Seuns-MacBook-Pro.local>
2021-04-15 12:59:38 -0500

    Route for adding new room to room options

--- backend/routes.js
+++ backend/routes.js
@@ -749,6 +749,72 @@ module.exports = function routes(app, logger) {
     
   });
 
+  // GET /api/rooms
+  //adds new room to the rooms
+  app.post('/api/rooms', (req, res) => {
+    // obtain a connection from our pool of connections
+    pool.getConnection(async function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection'); 
+      } else {
+        let userEmail = req.body["userEmail"];
+        let userPassword = req.body["userPassword"];
+        let roomId = req.body["roomId"];
+        let roomType = req.body["roomType"];
+        let capacity = req.body["capacity"];
+        let currentEmployees = 0;
+        let availability = req.body["availability"];
+        let cleaned = req.body["cleaned"];
+        let beingCleaned = req.body["beingCleaned"];
+        const hash = crypto.createHmac('sha256', secret).update(userPassword).digest('hex');
+        //credential check and returns back all requests with the necessary information needed to approve or deny the request
+        let sql = 'SELECT userId, jobTitle FROM users WHERE userEmail = \'' + userEmail + '\' AND userPassword=\'' + hash + '\'';
+        let values = [
+          [roomId, roomType, capacity, currentEmployees, availability, cleaned, beingCleaned]
+        ];
+        connection.query(sql, function (err, rows, fields) {
+          if (err) {
+            logger.error("Error while fetching values: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining values"
+            })
+          } else { 
+            if(rows.length > 0){
+              if(rows[0]["jobTitle"]){
+                sql = 'INSERT INTO rooms(roomId, roomType, capacity, currentEmployees, availability, cleaned, beingCleaned) VALUES ?';
+                connection.query(sql, [values], function (err, rows, fields) {
+                  connection.release();
+                  if (err) {
+                    logger.error("Error while fetching values: \n", err);
+                    res.status(400).json({
+                      "data": [],
+                      "error": "Error obtaining values"
+                    })
+                  } else { 
+                    //successful insert
+                    res.status(200).json({
+                      "status" : 0
+                    })
+                  }
+                });
+              }
+            }
+            else{
+              //not logged in or incorrect credentials
+              res.status(200).json({
+                "status": "1"
+              })
+            }
+          }
+        });
+      }
+    });
+    
+  });
+
   // GET /api/availableRegularRoom
   //returns all the available generalRooms
   app.get('/api/availableRegularRoom', (req, res) => {
@@ -940,19 +1006,19 @@ module.exports = function routes(app, logger) {
             })
           } else { 
             if(rows.length > 0){
-                sql = 'SELECT (r.capacity - '+
-                'CASE '+ 
-                    'WHEN r2.amount IS NOT NULL '+
-                        'THEN r2.amount ' +
-                    'ELSE 0 ' +
-                'END ' +   
-                ') as difference, r.roomId  FROM rooms r LEFT JOIN (SELECT COUNT(*) as amount, roomId FROM reservations WHERE dateIn <= \''+dateTime+'\' AND dateOut >= \''+ dateTime +'\' GROUP BY roomId ) r2 ON r.roomId = r2.roomId WHERE r.roomType = 2 AND (r.capacity - '+
-                'CASE '+
-                    'WHEN r2.amount IS NOT NULL '+
-                        'THEN r2.amount '+
-                    'ELSE 0 '+
-                'END '+    
-                ') >= '+capacity;     
+                sql = `SELECT (r.capacity - 
+                CASE 
+                    WHEN r2.amount IS NOT NULL 
+                        THEN r2.amount 
+                    ELSE 0 
+                END  
+                ) as difference, r.roomId  FROM rooms r LEFT JOIN (SELECT COUNT(*) as amount, roomId FROM reservations WHERE dateIn <= '${dateTime}' AND dateOut >= '${dateTime}' GROUP BY roomId ) r2 ON r.roomId = r2.roomId WHERE r.roomType = 2 AND (r.capacity - 
+                CASE 
+                    WHEN r2.amount IS NOT NULL 
+                        THEN r2.amount 
+                    ELSE 0 
+                END  
+                ) >= ${capacity}`    
                 console.log(sql)     
                 connection.query(sql, function (err, rows, fields) {
                   connection.release();
