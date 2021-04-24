1d4dc109dfe63f62638848cb091606b9489d0e5e
Logan Schmitt <logan.schmitt@me.com>
2021-04-24 14:46:38 -0500

    Implemented Profile button and more styling

--- frontend/src/components/employee-list/employee-list.js
+++ frontend/src/components/employee-list/employee-list.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Link } from "react";
 import Header from "../header";
 import { UserRepository } from "../../api/userRepository";
+import { useHistory } from 'react-router-dom';
 
 const EmployeeList = () => {
   
   const [users, setUsers] = useState(undefined);
+  const history = useHistory();
 
   useEffect(() => {
     const userRepository = new UserRepository(); 
@@ -24,57 +26,68 @@ const EmployeeList = () => {
     <div>
       <Header />
       <div class="container mb-4">
-        <table className="table table-striped">
-          <thead className="border-top-0">
+
+      { /* Start of Available Employee Table */}
+        <table className=" table table-condensed table-striped border">
+          <thead>
             <tr>
-              <th className="h3 table-success">Available Employees</th>
-              <th></th>
+              <th className="h3 table-success rounded-top">Available Employees</th>
             </tr>
+            
           </thead>
           <tbody>
             {users &&
               users.map((user) => {
-                console.log(user)
                 return (
-                  // user.status == 0 &&
-                  // user.exposure == false && 
-                  // Need to reimplement the checks
+                  user.status == 0 ||
+                  user.exposure == false && 
                   (
                     <tr key={user.userId}>
-                      <td>{user.userId + ' ' + user.firstName + ' ' + user.lastName}</td>
+                      <td>
+                        <span className='text-muted'>{user.userId}</span>  
+                        <span className='fs-1'>{' ' + user.firstName + ' ' + user.lastName}</span>
+                        <a className='btn btn-info float-right' onClick={() => history.push('/profile/' + user.userId)}>See Profile</a>
+                      </td>
                     </tr>
                   )
                 );
               })}
           </tbody>
         </table>
-        <table className="table table-striped">
-          <thead className="border-top-0">
+
+        { /* Start of Contact Traced Employee Table */}
+        <table className="table table-striped border">
+          <thead >
             <tr>
               <th className="h3 table-warning">Contact-Traced Employees</th>
-              <th></th>
+              
             </tr>
           </thead>
           <tbody>
             {users &&
               users.map((user) => {
                 return (
-                  // user.covidStatus == 0 &&
-                  // user.exposure == true && 
+                  user.status == 0 &&
+                  user.exposure == true && 
                   (
                     <tr key={user.userId}>
-                      <td>{user.userId + ' ' + user.firstName + ' ' + user.lastName}</td>
+                      <td>
+                        <span className='text-muted'>{user.userId}</span>  
+                        <span className='fs-1'>{' ' + user.firstName + ' ' + user.lastName}</span>
+                        <a className='btn btn-info float-right' onClick={() => history.push('/profile/' + user.userId)}>See Profile</a>
+                      </td>
                     </tr>
                   )
                 );
               })}
           </tbody>
         </table>
-        <table className="table table-striped">
-          <thead className="border-top-0">
+
+        { /* Start of Employees with Covid-19 Table */}
+        <table className="table table-striped border">
+          <thead >
             <tr>
               <th className="h3 table-danger">Employees with COVID-19</th>
-              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -83,7 +96,11 @@ const EmployeeList = () => {
                 return (
                   user.status == 1 && (
                     <tr key={user.userId}>
-                      <td>{user.userId + ' ' + user.firstName + ' ' + user.lastName}</td>
+                      <td>
+                        <span className='text-muted'>{user.userId}</span>  
+                        <span className='fs-1'>{' ' + user.firstName + ' ' + user.lastName}</span>
+                        <a className='btn btn-info float-right' onClick={() => history.push('/profile/' + user.userId)}>See Profile</a>
+                      </td>
                     </tr>
                   )
                 );
