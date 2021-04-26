dcf2be621b08b626685ba51c49ebf43ba4d5aa67
millerbm18 <millerbm@smu.edu>
2021-04-26 15:22:58 -0500

    added reservations to userProfile

--- frontend/src/components/user-profile/user-profile.js
+++ frontend/src/components/user-profile/user-profile.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { UserRepository } from '../../api/userRepository';
-//import { EmployeeRepository } from '../../api/employeeRepository';
+import { EmployeeRepository } from '../../api/employeeRepository';
 import Header from '../header';
 
 export const UserProfile = props => {
 
     const userRepository = new UserRepository();
+    const employeeRepository = new EmployeeRepository();
 
     const [user, setUser] = useState(undefined);
+    const [reservations, setReservations] = useState(undefined);
+    const [month, setMonth] = useState(new Date().getMonth() + 1);
+    const [year, setYear] = useState(new Date().getFullYear());
 
     const userId=props.match.params.userId;
 
@@ -15,20 +19,36 @@ export const UserProfile = props => {
 
     useEffect(()=>{
         if(!user){
-            console.log(userId);
             userRepository.getUserById(userId)
                 .then(response => {
-                    if(response[1].success===true){
-                        setUser(response[0].data[0])
+                    if(response[1].success){
+                        setUser(response[0].data[0]);
                     }
                     else{
-                        console.log("error fetching profile")
+                        console.log("error fetching profile "+userId);
                     }
                     });
         }
+        if(!reservations){
+            employeeRepository.getReservations(month, year)
+            .then((res) => {
+                if (res[1].success) {
+                    let resArr=[];
+                    res[0].data.forEach(element => {
+                        if(element.userId==userId){
+                            resArr.push(element);
+                        }
+                    });
+                    setReservations(resArr);
+                } else {
+                    console.log("error fetching reservations for "+new Date().getMonth()+" "+new Date().getFullYear());
+                    setReservations([]);
+                }
+              });
+        }
     });
 
-    if(!user){
+    if(!user || !reservations){
         return <>
         <div>Error: Profile userId:{userId} not found </div>
         </>
@@ -61,6 +81,30 @@ export const UserProfile = props => {
             <div className= "m-5">
                 <h5>Status</h5>
                 <p>{covidStatus}</p>
+            </div>
+            <div className= "m-5">
+                <h5>Reservations</h5>
+                <table className=" table table-condensed table-striped border">
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Room Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            reservations.map(reservation => 
+                            {
+                                return(
+                                    <tr key = {reservation.reservationId}>
+                                        <td>{new Date(reservation.dateIn).toDateString()}</td>
+                                        <td>{reservation.roomId}</td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table>
             </div>
         </div>
         </>;
