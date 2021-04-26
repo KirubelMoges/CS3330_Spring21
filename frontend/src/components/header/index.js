5a1f100225e24d3a61d877a2e694fb46dd997445
Nick Wall <walln@smu.edu>
2021-04-26 02:11:09 -0500

    Fixed route guards

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,21 +1,26 @@
-import { Navbar, Nav } from "react-bootstrap";
-import { useContext } from "react";
-import { UserContext } from "../../common/context";
-import { UserRepository } from "../../api/userRepository";
-import { useHistory } from "react-router";
+import { Navbar, Nav } from 'react-bootstrap';
+import { useContext } from 'react';
+import { UserContext } from '../../common/context';
+import { UserRepository } from '../../api/userRepository';
+import { useHistory } from 'react-router';
 
 const Header = () => {
   const userRepository = new UserRepository();
 
   return (
     <Navbar className="mr-auto bg-dark mb-2 navbar-dark" expand="lg">
-      <Navbar.Brand href="/" className='text-white'>Covid Workplace Planning Tool</Navbar.Brand>
+      <Navbar.Brand href="/" className="text-white">
+        Covid Workplace Planning Tool
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="navbar-nav" />
       <Navbar.Collapse id="navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/employees" className='text-white'>Employees</Nav.Link>
-          <Nav.Link href="/rooms" className='text-white'>Rooms</Nav.Link>
-          <Nav.Link href="/inbox" className='text-white'>Inbox</Nav.Link>
+          <Nav.Link href="/employees" className="text-white">
+            Employees
+          </Nav.Link>
+          <Nav.Link href="/rooms" className="text-white">
+            Rooms
+          </Nav.Link>
         </Nav>
         {userRepository.loggedIn() ? <LoggedInVariant /> : <LoggedOutVariant />}
       </Navbar.Collapse>
@@ -27,10 +32,14 @@ const LoggedOutVariant = () => {
   return (
     <Nav>
       <Nav.Item>
-        <Nav.Link href="/login" className='text-white'>Login</Nav.Link>
+        <Nav.Link href="/login" className="text-white">
+          Login
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/register" className='text-white'>Sign Up</Nav.Link>
+        <Nav.Link href="/register" className="text-white">
+          Sign Up
+        </Nav.Link>
       </Nav.Item>
     </Nav>
   );
@@ -43,13 +52,15 @@ const LoggedInVariant = () => {
   function logoutButton() {
     userRepository.logout();
     setUserContext({});
-    history.push("/login");
+    history.push('/login');
   }
 
   return (
     <Nav>
       <Nav.Item>
-        <Nav.Link onClick={logoutButton} className='text-white'>Logout</Nav.Link>
+        <Nav.Link onClick={logoutButton} className="text-white">
+          Logout
+        </Nav.Link>
       </Nav.Item>
     </Nav>
   );
